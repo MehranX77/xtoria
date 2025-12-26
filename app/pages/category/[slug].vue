a6ce7b0ee0f977f33fdb9a24362913ac4ee0117e
Mehran <mehranmehrabifar77@gmail.com>
2025-12-27 00:06:17 +0330

    ok

--- app/pages/category/[slug].vue
+++ app/pages/category/[slug].vue
@@ -8,15 +8,15 @@
                     </span>
                 </template>
             </UBreadcrumb>
-           <div class="flex gap-x-3">
-                    <div class="flex flex-col gap-y-2 mt-5">
+           <div class="flex lg:flex-nowrap flex-wrap gap-x-3">
+                    <div class="flex lg:flex-col gap-y-2 mt-5 lg:order-first order-last">
                      <UButton size="xl" variant="ghost" color="neutral" class="self-start text-2xl rounded-lg" icon="solar:share-line-duotone"/>
                      <UButton size="xl" variant="ghost" color="neutral" class="self-start text-2xl rounded-lg" icon="mage:telegram"/>
                      <UButton size="xl" variant="ghost" color="neutral" class="self-start text-2xl rounded-lg" icon="mage:instagram-square"/>
                     </div>
-            <div class="flex dark:bg-slate-800 bg-slate-100 w-full p-4 rounded-lg mt-5 gap-x-5">
+            <div class="flex md:flex-nowrap flex-wrap dark:bg-slate-800 bg-slate-100 w-full p-4 rounded-lg mt-5 gap-x-5">
                 <div class="gallery basis-110">
-                    <NuxtImg class="w-80 h-80 max-h-80 object-fit rounded-lg" :src="galleryRef" />
+                    <NuxtImg class="lg:w-80 lg:h-80 lg:max-h-80 w-40 h-40 max-h-40 mx-auto lg:mx-0 object-fit rounded-lg" :src="galleryRef" />
                     <div class="flex justify-center sub-img gap-x-5">
                         <NuxtImg v-for="(value, index) in galleryList" :key="index" class="w-20 h-20 max-h-20 hover:cursor-pointer transition-all object-fit rounded-lg border border-slate-300 dark:border-slate-700/90 hover:border-rose-700/90 p-2" :src="value" @click="changeImg(value)" />
                     </div>
@@ -35,21 +35,21 @@
                         <span class="self-center text-sky-500">Sony</span>
                        </div>
                         <ul class="space-y-2">
-                            <li class="text-xl"><UIcon class="align-middle me-1 text-2xl text-green-500" name="solar:check-circle-line-duotone"/>قابل استفاده به صورت بی‌سیم و با کابل USB-C</li>
-                            <li class="text-xl"><UIcon class="align-middle me-1 text-2xl text-green-500" name="solar:check-circle-line-duotone"/>قابل استفاده به صورت بی‌سیم و با کابل USB-C</li>
+                            <li class="lg:text-xl md:text-lg text-base"><UIcon class="align-middle me-1 text-2xl text-green-500" name="solar:check-circle-line-duotone"/>قابل استفاده به صورت بی‌سیم و با کابل USB-C</li>
+                            <li class="lg:text-xl md:text-lg text-base"><UIcon class="align-middle me-1 text-2xl text-green-500" name="solar:check-circle-line-duotone"/>قابل استفاده به صورت بی‌سیم و با کابل USB-C</li>
                         </ul>
-                        <div class="flex gap-x-6 mt-4">
-                            <UFormField orientation="horizontal" label="فروشنده" class="text-xl">
+                        <div class="flex lg:flex-nowrap flex-wrap lg:gap-x-6 gap-y-3 mt-4">
+                            <UFormField orientation="horizontal" label="فروشنده" class="lg:text-xl text-base">
                                 <USelect v-model="value" size="xl" :items="branch" class="w-full"/>
                             </UFormField>
-                            <UFormField orientation="horizontal" label="تعداد" class="text-xl">
+                            <UFormField orientation="horizontal" label="تعداد" class="lg:text-xl text-base">
                                  <UInputNumber v-model="deviceNumber" size="xl" :min="1" />
                             </UFormField>
                         </div>
-                        <UFormField label="انتخاب گارانتی" required class="space-y-4 text-xl">
+                        <UFormField label="انتخاب گارانتی" required class="space-y-4 lg:text-xl text-base">
                            <URadioGroup v-model="garantee" size="xl" dir="rtl" class="text-end w-fit " :items="garanteeList" />  
                         </UFormField>
-                         <UButton variant="solid" size="xl" color="success" class="w-[40%] text-xl my-3 place-content-center" trailing icon="solar:cart-large-2-line-duotone">76000 تومن</UButton>
+                         <UButton variant="solid" size="xl" color="success" class="lg:w-[40%] w-full text-xl my-3 place-content-center" trailing icon="solar:cart-large-2-line-duotone">76000 تومن</UButton>
                     </div>
                     </div>
                 </div>
@@ -62,21 +62,21 @@
             </div>
            </div>
            <USeparator color="neutral" class="mt-10"/>
-           <div class="flex justify-around mt-5">
+           <div class="flex lg:flex-nowrap flex-wrap lg:justify-around lg:gap-y-0 gap-y-3 mt-5">
             <div class="flex gap-x-2">
-                <UIcon class="dark:text-slate-600/70 text-5xl " name="mdi:truck-fast-outline"/>
+                <UIcon class="dark:text-slate-600/70 lg:text-5xl md:text-3xl text-lg" name="mdi:truck-fast-outline"/>
                 <span class="dark:text-slate-600/70 text-md  self-center">امکان تحویل اکسپرس</span>
             </div>
               <div class="flex gap-x-2">
-                <UIcon class="dark:text-slate-600/70 text-5xl " name="mdi:headset"/>
+                <UIcon class="dark:text-slate-600/70 lg:text-5xl md:text-3xl text-lg" name="mdi:headset"/>
                 <span class="dark:text-slate-600/70 text-md  self-center">پشتیبانی آنلاین</span>
             </div>
             <div class="flex gap-x-2">
-                <UIcon class="dark:text-slate-600/70 text-5xl " name="streamline-ultimate:cash-payment-bills"/>
+                <UIcon class="dark:text-slate-600/70 lg:text-5xl md:text-3xl text-lg" name="streamline-ultimate:cash-payment-bills"/>
                 <span class="dark:text-slate-600/70 text-md  self-center">پرداخت درب محل</span>
             </div>
              <div class="flex gap-x-2">
-                <UIcon class="dark:text-slate-600/70 text-5xl " name="solar:medal-ribbon-star-outline"/>
+                <UIcon class="dark:text-slate-600/70 lg:text-5xl md:text-3xl text-lg" name="solar:medal-ribbon-star-outline"/>
                 <span class="dark:text-slate-600/70 text-md  self-center">ضمانت اصل بودن  کالا</span>
             </div>
            </div>
