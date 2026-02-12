f58648f58fc1998ad4e6fce17d2bde5c3a691686
Mehran <mehranmehrabifar77@gmail.com>
2026-02-12 11:37:27 +0330

    ok

--- app/pages/user/support.vue
+++ app/pages/user/support.vue
@@ -22,7 +22,7 @@
                                  <UFormField required label="متن تیکت" class="mt-3">
                                     <UTextarea variant="outline" size="xl" placeholder="مثال: پیگیری سفارش" class="w-full mt-2" color="neutral" :rows="10" />
                                 </UFormField>  
-                                <UButton type="submit" color="info" variant="subtle" size="xl" class="mt-3">ارسال تیکت</UButton> 
+                                <UButton type="submit" color="neutral" variant="subtle" size="xl" class="mt-3">ارسال تیکت</UButton> 
                                </UForm> 
                             </template>
                         </UCard>
@@ -45,6 +45,36 @@
                     </template>
                 </UModal>
             </div>
+            <!-- باکس پرسش و پاسخ مشتریان -->
+             
+                <div class="flex md:w-4/5 md:mx-auto w-full mt-16">
+                    <UCard variant="soft" class="w-full">
+                        <template #header>
+                            <div class="flex justify-between">
+                                <div class="flex flex-col gap-y-4">
+                                    <h1 class="text-2xl">تیکت تستی</h1>
+                                    <span class="text-muted text-md">ایجاد شده در چهارشنبه 22 بهمن 1404</span>
+                                </div>
+                                <div class="flex flex-col gap-y-4">
+                                 <UBadge variant="outline" color="warning" size="xl">در انتظار پاسخ واحد فروش</UBadge>
+                                 <span class="text-muted text-sm">آیدی تیکت : #14256</span>
+                                </div>
+                            </div>
+                        </template>
+                        <template #default>
+                                <UTextarea variant="subtle" color="neutral" placeholder="متن تیکت" :rows="10" class="w-full"/>
+                                <div class="flex justify-between mt-6">
+                                     <div class="flex flex-col gap-y-3">
+                                        <p class="dark:text-neutral-300 text-neutral-700 text-base">پیوست‌ها</p>
+                                        <span class="text-muted text-sm">اگر تصویر محصول خریداری شده دارید که برای حل مشکل به ما کمک می‌کند. لطفا آن را پیوست کنید</span>
+                                        <UFileUpload accept="image/*" label="فایل پیوست خود را وارد کنید" description="(png, jpeg)" icon="i-lucide-image" class="w-96"/>
+                                     </div>
+                                     <UButton class="self-baseline" size="xl" color="neutral" variant="subtle">ارسال پاسخ</UButton>
+                                </div>
+                        </template>
+                    </UCard>
+                </div>
+            
         </div>
     </div>
 </template>
